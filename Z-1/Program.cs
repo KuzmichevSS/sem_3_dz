468accbaf71ec90d4d0f309fd04fe0f9eb9b96cf
Sergey Kuzmichev <kyz1979@mail.ru>
2023-02-04 00:37:01 +0300

    задача 1

---
+++ Z-1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int i = num / 10000;
+int b = num % 10;
+if (i==b)
+{
+    Console.WriteLine("Палиодром");
+}
+else
+{
+    Console.WriteLine("Не палиодрон");
+
+}
